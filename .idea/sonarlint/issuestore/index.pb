bf5a04ee6a5a99d29d29829243970b9c5472c799
Prateek Vijay <prateek.vijay@coviam.com>
2020-04-26 23:27:21 +0530

    Navigation graph setup for Authentication

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+e
+5app/src/main/java/com/example/openapi/MainActivity.kt,e/2/e2578c9459f4d81e4da976abd678b435483ac185
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ app/src/main/java/com/example/openapi/ui/auth/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.openapi.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.openapi.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
